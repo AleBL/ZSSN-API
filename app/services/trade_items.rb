bf5d32c7a77f8adfa2957ffb7741bbfd95e9a268
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-05 20:49:16 -0300

    trade items services created

---
+++ app/services/trade_items.rb
@@ -0,0 +1,36 @@
+class TradeItems
+  attr_reader :inventory_from, :inventory_to, :trade_params
+
+  def initialize(inventory_from:, inventory_to:, trade_params:)
+    @inventory_from = inventory_from
+    @inventory_to   = inventory_to
+    @trade_params   = trade_params
+  end
+
+  def valid?
+    false unless valid_trade?
+
+    ActiveRecord::Base.transaction do
+      perform_trade
+    end
+  end
+
+  private
+
+  def valid_trade?
+    TradeValidator.new(inventory_from: inventory_from, inventory_to: inventory_to).valid?
+  end
+
+  def perform_trade
+    item_trader.remove_from
+    item_trader.add_to
+  end
+
+  def item_trader
+    @item_trader ||= ItemTrader.new(inventory_from: inventory_from,
+                                    inventory_to:   inventory_to,
+                                    trade_params:   trade_params)
+  end
+
+
+end
