1ca40d40b8f87ea36a82c655c316e10c6ac984fd
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-05 20:20:57 -0300

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+50.times do
+  inventory = Inventory.create(
+    water:      FFaker::Random.rand(0..99),
+    food:       FFaker::Random.rand(0..99),
+    medication: FFaker::Random.rand(0..99),
+    ammunition: FFaker::Random.rand(0..99)
+  )
+
+  if inventory.persisted?
+      person = Person.create(
+        name:     FFaker::NameBR.name,
+        age:      FFaker::Random.rand(1..99),
+        gender:   FFaker::GenderBR.maybe,
+        local:    FFaker::Geolocation.lat.to_s + ", " + FFaker::Geolocation.lng.to_s,
+        infected: false,
+        inventory_id: inventory.id
+      )
+  end
+end
