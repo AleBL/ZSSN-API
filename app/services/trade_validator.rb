873396317a5efa389b7d335add7c70a53b78d927
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-05 20:48:06 -0300

    trade validator services created

---
+++ app/services/trade_validator.rb
@@ -0,0 +1,30 @@
+class TradeValidator
+  attr_reader :inventory_from, :inventory_to
+
+  def initialize(inventory_from:, inventory_to:)
+    @inventory_from = inventory_from
+    @inventory_to   = inventory_to
+  end
+
+  def valid?
+    false if infected?
+
+    inventory_from_points == inventory_to_points
+  end
+
+  private
+  def infected?
+    person_from = Person.where(inventory_id: inventory_from.id)[0]
+    person_to   = Person.where(inventory_id: inventory_to.id)[0]
+
+    (person_from.infected? || person_to.infected?)
+  end
+
+  def inventory_from_points
+    InventoryCalculator.new(inventory: inventory_from).total
+  end
+
+  def inventory_to_points
+    InventoryCalculator.new(inventory: inventory_to).total
+  end
+end
