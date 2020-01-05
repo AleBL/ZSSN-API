fb3a4054e410d31a735ad802601d04ed3cbed849
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-05 20:19:49 -0300

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    resources :people
+    resources :locations
+    resources :report_people
+    resources :trade_items
+    resources :reports
+  end
 end
