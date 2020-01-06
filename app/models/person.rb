74e316a45811440bea707c92ec3de4b5bff4edcb
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-06 17:51:34 -0300

    location validation added

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,14 @@
 class Person < ApplicationRecord
   validates :name, presence: true
+  validates :local, presence: true
   validates :inventory_id, presence: true
+
+  validate :check_local
+  def check_local
+    location = local.split(',');
+
+    errors.add(:local, "location is invalid") if location.length > 2
+    errors.add(:local, "location is invalid") if location[0].to_f == 0.0
+    errors.add(:local, "location is invalid") if location[1].to_f == 0.0
+  end
 end
