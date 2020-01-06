f3e5b2c928a26fd72654b6c49becb15a83cc7c4a
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-05 23:14:10 -0300

    people factory attribute infected added

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :person do
-    name   { FFaker::NameBR.name }
-    age    { FFaker::Random.rand(1..99) }
-    gender { FFaker::GenderBR.maybe }
-    local  { FFaker::Geolocation.lat.to_s + ", " + FFaker::Geolocation.lng.to_s }
+    name     { FFaker::NameBR.name }
+    age      { FFaker::Random.rand(1..99) }
+    gender   { FFaker::GenderBR.maybe }
+    infected { false }
+    local    { FFaker::Geolocation.lat.to_s + ", " + FFaker::Geolocation.lng.to_s }
   end
 end
