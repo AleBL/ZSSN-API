7e9a37bcb8ec4b10f9dfd0bb091d23717b264a24
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-06 17:56:39 -0300

    'local' parameter check removed

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -4,10 +4,10 @@ module Api
       person = Person.find(params[:id])
       local = params.permit(:local)
 
-      if (person.update_attributes(local) && !local.empty?)
+      if (person.update_attributes(local))
         render json: {status: 'SUCCESS', message: 'Your Location was successfully updated'}, status: :ok
       else
-        render json: {status: 'ERROR', message: 'Your Location wasn`t updated'}, status: :unprocessable_entity
+        render json: {status: 'ERROR', message: 'Your Location wasn`t updated', data: person.errors}, status: :unprocessable_entity
       end
     end
   end
