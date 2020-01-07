16c21ff912f3e0df3049a5d0b1c62e6405f2079b
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-07 18:24:39 -0300

    correction factory bot people

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -28,7 +28,7 @@ FactoryBot.define do
     name         { FFaker::NameBR.name }
     age          { FFaker::Random.rand(1..99) }
     gender       { FFaker::GenderBR.maybe }
-    infected     { false }
+    infected     { true }
     local        { FFaker::Geolocation.lat.to_s + ", " + FFaker::Geolocation.lng.to_s }
     inventory_id { FactoryBot.create(:inventory_trade).id }
   end
