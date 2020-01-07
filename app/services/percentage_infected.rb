61bab0a10613413fef6779fe3e16f85d2f354b7c
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-07 00:16:28 -0300

    percentage infected service created

---
+++ app/services/percentage_infected.rb
@@ -0,0 +1,16 @@
+class PercentageInfected
+  def percentage
+    @result ||= (infected_count/total_count.to_f) * 100
+    @result.round(2)
+  end
+
+  private
+  
+  def infected_count
+    @infected_count ||= Person.where(infected: true).count
+  end
+
+  def total_count
+    @total_count ||= Person.count
+  end
+end
