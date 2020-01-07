af7eaf9bddedf82825a8e82c6e06d17aef532b5d
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-07 00:14:38 -0300

    fixing trade validator

--- app/services/trade_validator.rb
+++ app/services/trade_validator.rb
@@ -1,30 +1,49 @@
 class TradeValidator
-  attr_reader :inventory_from, :inventory_to
+  attr_reader :inventory_from, :inventory_to, :trade_params
 
-  def initialize(inventory_from:, inventory_to:)
+  def initialize(inventory_from:, inventory_to:, trade_params:)
     @inventory_from = inventory_from
     @inventory_to   = inventory_to
+    @trade_params   = trade_params
   end
 
   def valid?
-    false if infected?
+    return false if infected?
 
     inventory_from_points == inventory_to_points
   end
 
-  private
+  # private
   def infected?
     person_from = Person.where(inventory_id: inventory_from.id)[0]
     person_to   = Person.where(inventory_id: inventory_to.id)[0]
 
-    (person_from.infected? || person_to.infected?)
+    person_from.infected? || person_to.infected?
   end
 
   def inventory_from_points
-    ItemsCalc.new(inventory: inventory_from).total
+    params_from    = trade_params[:inventory_from]
+
+    inventory_from_trade = Inventory.new do |i|
+      i.water      = params_from["water"]
+      i.food       = params_from["food"]
+      i.medication = params_from["medication"]
+      i.ammunition = params_from["ammunition"]
+    end
+
+    ItemsCalc.new(inventory: inventory_from_trade).total
   end
 
   def inventory_to_points
-    ItemsCalc.new(inventory: inventory_to).total
+    params_to       = trade_params[:inventory_to]
+
+    inventory_to_trade = Inventory.new do |i|
+      i.water      = params_to["water"]
+      i.food       = params_to["food"]
+      i.medication = params_to["medication"]
+      i.ammunition = params_to["ammunition"]
+    end
+
+    ItemsCalc.new(inventory: inventory_to_trade).total
   end
 end
