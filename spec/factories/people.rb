eadfcba5fca6bfc0462f7d68c9e08fa632fc9860
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-07 10:41:03 -0300

    new functions in factory people created

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -6,4 +6,31 @@ FactoryBot.define do
     infected { false }
     local    { FFaker::Geolocation.lat.to_s + ", " + FFaker::Geolocation.lng.to_s }
   end
+
+  factory :person_infected, class: 'Person' do
+    name     { FFaker::NameBR.name }
+    age      { FFaker::Random.rand(1..99) }
+    gender   { FFaker::GenderBR.maybe }
+    infected { true }
+    local    { FFaker::Geolocation.lat.to_s + ", " + FFaker::Geolocation.lng.to_s }
+  end
+
+  factory :person_with_infectory, class: 'Person'do
+    name         { FFaker::NameBR.name }
+    age          { FFaker::Random.rand(1..99) }
+    gender       { FFaker::GenderBR.maybe }
+    infected     { false }
+    local        { FFaker::Geolocation.lat.to_s + ", " + FFaker::Geolocation.lng.to_s }
+    inventory_id { FactoryBot.create(:inventory).id }
+  end
+
+  factory :person_infected_with_infectory, class: 'Person'do
+    name         { FFaker::NameBR.name }
+    age          { FFaker::Random.rand(1..99) }
+    gender       { FFaker::GenderBR.maybe }
+    infected     { false }
+    local        { FFaker::Geolocation.lat.to_s + ", " + FFaker::Geolocation.lng.to_s }
+    inventory_id { FactoryBot.create(:inventory).id }
+  end
+  
 end
