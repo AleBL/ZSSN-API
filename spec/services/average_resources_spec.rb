d8ae65dd08227de4234ec144529558bb3ab5edf2
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-07 18:25:14 -0300

    services average resources spec created

---
+++ spec/services/average_resources_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe AverageResources do
+  describe "average resources" do
+    let(:average_resources)   { AverageResources.new }
+    let(:people_not_infected) { Person.where(infected: false) }
+    let(:inventories_people)  {
+      people_not_infected.inject({ water: 0, food: 0, medication: 0, ammunition: 0 }) do |hash, person|
+        inventory = Inventory.find(person.inventory_id)
+  
+        hash[:water]      += inventory.water
+        hash[:food]       += inventory.food
+        hash[:medication] += inventory.medication
+        hash[:ammunition] += inventory.ammunition
+        hash
+      end
+    }
+
+    context "valid results" do
+      it "average items is valid" do
+        average_items = average_resources.average_items
+        result_spec = (inventories_people[:water] / people_not_infected.count.to_f) +
+                      (inventories_people[:food] / people_not_infected.count.to_f) +
+                      (inventories_people[:medication] / people_not_infected.count.to_f) +
+                      (inventories_people[:ammunition] / people_not_infected.count.to_f)
+        expect(average_items).to eq(result_spec)
+      end
+
+      it "average water is valid" do
+        average_water = average_resources.average_water
+        result_spec =  (inventories_people[:water] / people_not_infected.count.to_f)
+        expect(average_water).to eq(result_spec)
+      end
+
+      it "average food is valid" do
+        average_food = average_resources.average_food
+        result_spec  = inventories_people[:food] / people_not_infected.count.to_f
+        expect(average_food).to eq(result_spec)
+      end
+
+      it "average medication is valid" do
+        average_medication = average_resources.average_medication
+        result_spec = inventories_people[:medication] / people_not_infected.count.to_f
+        expect(average_medication).to eq(result_spec)
+      end
+
+      it "average ammunition is valid" do
+        average_ammunition = average_resources.average_ammunition
+        result_spec = inventories_people[:ammunition] / people_not_infected.count.to_f
+        expect(average_ammunition).to eq(result_spec)
+      end
+
+    end
+
+    context "invalid results" do
+      it "total is invalid" do
+        average_items = average_resources.average_items
+        result_spec = (inventories_people[:water] / people_not_infected.count.to_f) +
+                      (inventories_people[:food] / people_not_infected.count.to_f) +
+                      (inventories_people[:medication] / people_not_infected.count.to_f)
+        expect(average_items).to_not eq(result_spec)
+      end
+    end
+  end
+end
