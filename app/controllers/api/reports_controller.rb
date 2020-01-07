90ee64268e36703d37d5cbf8f67773086d3e0473
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-07 00:22:18 -0300

    reports controller created

--- app/controllers/api/reports_controller.rb
+++ app/controllers/api/reports_controller.rb
@@ -1,8 +1,36 @@
 module Api
-  class ReportController < ApplicationController
+  class ReportsController < ApplicationController
     def index
+      percentage_non_infected = ::PercentageNonInfected.new
 
-      render json: {status: 'SUCCESS'}, status: :ok
+      percentage_infected = ::PercentageInfected.new
+
+      average_resources = ::AverageResources.new
+
+      average_inventory = {
+        average_items:{
+          average_water: average_resources.average_water.round(2),
+          average_food: average_resources.average_food.round(2),
+          average_medication: average_resources.average_medication.round(2),
+          average_ammunition: average_resources.average_ammunition.round(2)
+        },
+        average_total: average_resources.average_items.round(2)
+      }
+
+      lost_points = ::PointsLost.new
+
+      lost_points_items = {
+        lost_points: lost_points.lost_points,
+        total: lost_points.total
+      }
+
+      reports = {
+        percentage_non_infected: "#{percentage_non_infected.percentage}%",
+        percentage_infected: "#{percentage_infected.percentage}%",
+        average_items_non_infected: average_inventory,
+        lost_points: lost_points_items
+      }
+      render json: {status: 'SUCCESS', message: 'Reports', data: reports}, status: :ok
     end
   end
 end
