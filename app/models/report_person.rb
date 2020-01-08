9ae0c2c58999334b4fd7457971937c65df8c20f5
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-08 10:36:26 -0300

    correction in uniqueness validation of report person model

--- app/models/report_person.rb
+++ app/models/report_person.rb
@@ -2,10 +2,14 @@ class ReportPerson < ApplicationRecord
   belongs_to :report_to, :class_name => 'Person'
   belongs_to :report_from, :class_name => 'Person'
 
-  validates_uniqueness_of :report_to, scoped: :report_from
+  validate :check_report_exist
   validate :check_report_equal
 
   def check_report_equal
     errors.add(:report_from, "can't be the same as report") if report_to == report_from
+  end
+
+  def check_report_exist
+    errors.add(:report_from, "report already exists") if ReportPerson.where(report_to: report_to, report_from: report_from).count > 0
   end
 end
