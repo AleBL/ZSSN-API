f483a8c74a18f3d6a38ddacf5c1fa4c0a9e921b9
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-08 09:45:57 -0300

    new tests created in model person spec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,19 +2,27 @@ require "rails_helper"
 
 RSpec.describe Person, type: :model do
   context "Valid Person" do
+    let(:person) { create(:person_with_inventory) }
     it "It is a valid person" do
-      person    = build(:person)
-      inventory = create(:inventory)
-      person.inventory_id = inventory.id
-
       expect(person.valid?).to be_truthy
     end
   end
 
   context "Invalid Person" do
+    let(:person) { create(:person_with_inventory) }
+    
     it "It is not a valid person, person doesn`t have a inventory" do
-      person = build(:person)
+      person.inventory_id = nil
+      expect(person.valid?).to be_falsy
+    end
 
+    it "It is not a valid person, latitude is not valid" do
+      person.local = FFaker::Geolocation.lat.to_s + ", " + FFaker::Lorem.sentence
+      expect(person.valid?).to be_falsy
+    end
+
+    it "It is not a valid person, longitude is not valid" do
+      person.local =  FFaker::Lorem.sentence + ", " + FFaker::Geolocation.lng.to_s
       expect(person.valid?).to be_falsy
     end
   end
