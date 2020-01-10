0b6e68e5c0bdec7dec6f6e35632f4a5091dcda78
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-10 00:19:53 -0300

    updating hash syntax

--- app/models/report_person.rb
+++ app/models/report_person.rb
@@ -1,6 +1,6 @@
 class ReportPerson < ApplicationRecord
-  belongs_to :report_to, :class_name => 'Person'
-  belongs_to :report_from, :class_name => 'Person'
+  belongs_to :report_to, class_name: 'Person'
+  belongs_to :report_from, class_name: 'Person'
 
   validate :check_report_exist
   validate :check_report_equal
