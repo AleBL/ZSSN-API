01733944465659afdb49f0cca6a0a00d84e0fe8c
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-09 23:27:10 -0300

    creating a new service for reduce complexity

--- app/services/points_lost.rb
+++ app/services/points_lost.rb
@@ -1,47 +1,28 @@
 class PointsLost
-  WEIGHTS = { water: 4, food: 3, medication: 2, ammunition: 1 }
-  attr_reader :people
+  attr_reader :people, :points_calc
 
   def initialize(params = {})
     @people = params.fetch(:people, Person.where(infected: true))
+    @points_calc = PointsCalc.new(people: people)
   end
 
   def total
-    @total ||= water_points + food_points + medication_points + ammunition_points
+    points_calc.total
   end
 
   def water_points
-    total_points[:water] * WEIGHTS[:water]
+    points_calc.water_points
   end
 
   def food_points
-    total_points[:food] * WEIGHTS[:food]
+    points_calc.food_points
   end
 
   def medication_points
-    total_points[:medication] * WEIGHTS[:medication]
+    points_calc.medication_points
   end
 
   def ammunition_points
-    total_points[:ammunition] * WEIGHTS[:ammunition]
-  end
-
-  private
-
-  def total_points
-    return @total_points if defined?(@total_points)
-    @total_points = people.inject(default_hash) do |hash, person|
-      inventory = Inventory.find(person.inventory_id)
-
-      hash[:water]      += inventory.water
-      hash[:food]       += inventory.food
-      hash[:medication] += inventory.medication
-      hash[:ammunition] += inventory.ammunition
-      hash
-    end
-  end
-
-  def default_hash
-    { water: 0, food: 0, medication: 0, ammunition: 0 }
+    points_calc.ammunition_points
   end
 end
--- spec/services/points_lost_spec.rb
+++ spec/services/points_lost_spec.rb
@@ -3,52 +3,37 @@ require 'rails_helper'
 RSpec.describe PointsLost do
   describe "points lost" do
     let(:points_lost)   { PointsLost.new }
-    let(:weights) {{ water: 4, food: 3, medication: 2, ammunition: 1 }}
     let(:people_infected) { Person.where(infected: true) }
-    let(:inventories_people)  {
-      people_infected.inject({ water: 0, food: 0, medication: 0, ammunition: 0 }) do |hash, person|
-        inventory = Inventory.find(person.inventory_id)
-  
-        hash[:water]      += inventory.water
-        hash[:food]       += inventory.food
-        hash[:medication] += inventory.medication
-        hash[:ammunition] += inventory.ammunition
-        hash
-      end
-    }
 
     context "valid results" do
       it "points lost total is valid" do
         points_lost_total = points_lost.total
-        result_spec = (inventories_people[:water] * weights[:water]) +
-                      (inventories_people[:food] * weights[:food])  +
-                      (inventories_people[:medication] * weights[:medication]) +
-                      (inventories_people[:ammunition] * weights[:ammunition])
+        result_spec = PointsCalc.new(people: people_infected).total
 
         expect(points_lost_total).to eq(result_spec)
       end
 
       it "points lost water is valid" do
         points_lost_water = points_lost.water_points
-        result_spec =  inventories_people[:water] * weights[:water]
+        result_spec = PointsCalc.new(people: people_infected).water_points
         expect(points_lost_water).to eq(result_spec)
       end
 
       it "points lost food is valid" do
         points_lost_food = points_lost.food_points
-        result_spec  = inventories_people[:food] * weights[:food]
+        result_spec  = PointsCalc.new(people: people_infected).food_points
         expect(points_lost_food).to eq(result_spec)
       end
 
       it "points lost medication is valid" do
         points_lost_medication = points_lost.medication_points
-        result_spec = inventories_people[:medication] * weights[:medication]
+        result_spec = PointsCalc.new(people: people_infected).medication_points
         expect(points_lost_medication).to eq(result_spec)
       end
 
       it "points lost ammunition is valid" do
         points_lost_ammunition = points_lost.ammunition_points
-        result_spec = inventories_people[:ammunition] * weights[:ammunition]
+        result_spec = PointsCalc.new(people: people_infected).ammunition_points
         expect(points_lost_ammunition).to eq(result_spec)
       end
 
@@ -57,12 +42,9 @@ RSpec.describe PointsLost do
     context "invalid results" do
       it "points lost total is invalid" do
         points_lost_total = points_lost.total
-        result_spec = (inventories_people[:water] * weights[:water]) +
-                      (inventories_people[:food] * weights[:food])  +
-                      (inventories_people[:medication] * weights[:medication]) +
-                      (inventories_people[:ammunition] * weights[:ammunition]) + 1
+        result_not_spec = PointsCalc.new(people: people_infected).total - rand(1..99)
 
-        expect(points_lost_total).to_not eq(result_spec)
+        expect(points_lost_total).to_not eq(result_not_spec)
       end
     end
   end
