e4923383995c56ac909bf6e8900dce4f104b457d
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-09 23:24:12 -0300

    service people creator spec created

---
+++ spec/services/people_creator_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe PeopleCreator do
+  describe "people creator" do
+    context "#created? true" do
+      let(:person_params_valid) {{
+        person: {
+          name: FFaker::NameBR.name,
+          age: FFaker::Random.rand(1..99),
+          local: FFaker::Geolocation.lat.to_s + ", " + FFaker::Geolocation.lng.to_s,
+          gender: FFaker::GenderBR.maybe
+        },
+        inventory: {
+          water: FFaker::Random.rand(1..99),
+          food: FFaker::Random.rand(1..99),
+          medication: FFaker::Random.rand(1..99),
+          ammunition: FFaker::Random.rand(1..99)
+        }
+      }}
+
+      it "person created" do
+        people_creator = PeopleCreator.new(create_person_params: person_params_valid)
+        expect(people_creator.created?).to be_truthy
+      end
+    end
+    
+    context "#created? false" do
+      let(:person_params_without_name) {{
+        person: {
+          age: FFaker::Random.rand(1..99),
+          local: FFaker::Geolocation.lat.to_s + ", " + FFaker::Geolocation.lng.to_s,
+          gender: FFaker::GenderBR.maybe
+        },
+        inventory: {
+          water: FFaker::Random.rand(1..99),
+          food: FFaker::Random.rand(1..99),
+          medication: FFaker::Random.rand(1..99),
+          ammunition: FFaker::Random.rand(1..99)
+        }
+      }}
+
+      it "person without name" do
+        people_creator = PeopleCreator.new(create_person_params: person_params_without_name)
+        expect(people_creator.created?).to be_falsey
+      end
+
+      let(:person_params_without_inventory) {{
+        person: {
+          name: FFaker::NameBR.name,
+          age: FFaker::Random.rand(1..99),
+          local: FFaker::Geolocation.lat.to_s + ", " + FFaker::Geolocation.lng.to_s,
+          gender: FFaker::GenderBR.maybe
+        }
+      }}
+
+      it "person without inventory" do
+        people_creator = PeopleCreator.new(create_person_params: person_params_without_inventory)
+        expect(people_creator.created?).to be_falsey
+      end
+    end
+  end
+end
