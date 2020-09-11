9bc662ea8b3184a7dd1a551440bd8456a293bf7d
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-09-11 12:51:20 -0300

    refactor report_people

--- app/services/report_people.rb
+++ app/services/report_people.rb
@@ -7,23 +7,26 @@ class ReportPeople
   end
 
   def valid?
+    return false unless report_to && report_from
     return false if report_to.infected? || report_from.infected?
 
     report_not_infected
   end
 
   def report_not_infected
-    report             = ReportPerson.new()
-    report.report_to   = report_to
-    report.report_from = report_from
-
-    report.save
+    report = ReportPerson.new(report_to: report_to, report_from: report_from)
+    
+    return false unless report.save
+    
+    verify_reports
+    true
   end
 
   def verify_reports
-    reports = ReportPerson.where(report_from: report_from.id)
+    reports = ReportPerson.where(report_from: report_from)
+
     if(reports.length >= 3)
-      report_from.update_attributes({"infected":true})
+      report_from.update_attributes(infected: true)
     end
   end
 
