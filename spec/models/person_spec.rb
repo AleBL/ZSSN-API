123df0cdb28c0028439768886e8e63b115c969c0
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-09-11 13:13:30 -0300

    added specs in models/person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -5,31 +5,32 @@ RSpec.describe Person, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:local) }
-    it { should validate_presence_of(:inventory_id) }
+
+    it { should belong_to(:inventory) }
   end
 
   context "Valid Person" do
-    let(:person) { create(:person_with_inventory) }
+    let(:person) { build(:person_with_inventory) }
     it "It is a valid person" do
       expect(person.valid?).to be_truthy
     end
   end
 
   context "Invalid Person" do
-    let(:person) { create(:person_with_inventory) }
+    let(:person) { build(:person_with_inventory) }
     
     it "It is not a valid person, person doesn`t have an inventory" do
-      person.inventory_id = nil
+      person.inventory = nil
       expect(person.valid?).to be_falsy
     end
 
     it "It is not a valid person, latitude is not valid" do
-      person.local = FFaker::Geolocation.lat.to_s + ", " + FFaker::Lorem.sentence
+      person.local = "#{FFaker::Geolocation.lat.to_s}, #{FFaker::Lorem.sentence}"
       expect(person.valid?).to be_falsy
     end
 
     it "It is not a valid person, longitude is not valid" do
-      person.local =  FFaker::Lorem.sentence + ", " + FFaker::Geolocation.lng.to_s
+      person.local = "#{FFaker::Lorem.sentence}, #{FFaker::Geolocation.lng.to_s}"
       expect(person.valid?).to be_falsy
     end
   end
